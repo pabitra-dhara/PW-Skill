1e6ae2cf08d769e6fd769f62872f8bf6b09a13f3
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-01 22:39:09 +0530

    add

--- Practice/PabitraCode/Stacks/DisplayStack.java
+++ Practice/PabitraCode/Stacks/DisplayStack.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DisplayStack {
+
+    static void displayReverse(Stack<Integer> s){
+        if(s.size()==0) return;
+        int top=s.pop();
+        System.out.print(top+" ");
+        displayReverse(s);
+        s.push(top);
+    }
+    static void displayrec(Stack<Integer> s){
+        if(s.size()==0) return;
+        int top=s.pop();
+        displayrec(s);
+        System.out.print(top+" ");
+        s.push(top);
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Stack<Integer> st=new Stack<>();
@@ -11,24 +26,29 @@ public class DisplayStack {
         st.push(4);
         st.push(5);
         System.out.println(st);
-        Stack<Integer> gt=new Stack<>();
-        while(st.size()>0){
-            gt.push(st.pop());
-        }
-        while(gt.size()>0){
-            int x=gt.pop();
-            System.out.print(x+" ");
-            st.push(x);
-        }
-        System.out.println(st);
+        displayReverse(st);
+        
+        displayrec(st);
+        // Stack<Integer> gt=new Stack<>();
+        // while(st.size()>0){
+        //     gt.push(st.pop());
+        // }
+        // while(gt.size()>0){
+        //     int x=gt.pop();
+        //     System.out.print(x+" ");
+        //     st.push(x);
+        // }
+        // System.out.println(st);
 
         //use array print stack value
-        int arr[]=new int[st.size()];
-        for(int i=st.size();i<=1;i++){
-            arr[i]=st.pop();
-        }
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+
+        // int arr[]=new int[st.size()];
+        // for(int i=st.size()-1;i>=0;i--){
+        //     arr[i]=st.pop();
+        // }
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+" ");
+        //     st.push(arr[i]);
+        // }
     }
 }
--- Practice/PabitraCode/Stacks/ReverseStack.java
+++ Practice/PabitraCode/Stacks/ReverseStack.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class ReverseStack {
+    public static void pushAtBottom(Stack<Integer> s,int x){
+        if(s.size()==0){ s.push(x); return;}
+        int top=s.pop();
+        pushAtBottom(s, x);
+        s.push(top);
+    }
+    public static void reverceRec(Stack<Integer> s){
+        if(s.size()==1) return;
+        int top=s.pop();
+        reverceRec(s);
+        pushAtBottom(s,top);
+    }
     public static void main(String[] args) {
         Stack<Integer> st=new Stack<>();
         Scanner sc=new Scanner(System.in);
@@ -12,10 +24,12 @@ public class ReverseStack {
             st.push(i=sc.nextInt());
         }
         System.out.println(st);
-        Stack<Integer> rs=new Stack<>();
-        while(st.size()>0){
-           rs.push(st.pop());
-        }
-        System.out.println(rs);
+        reverceRec(st);
+        System.out.println(st);
+        // Stack<Integer> rs=new Stack<>();
+        // while(st.size()>0){
+        //    rs.push(st.pop());
+        // }
+        // System.out.println(rs);
     }
 }
