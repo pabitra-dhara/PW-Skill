6b5d3e1df16dec378fa98f4e3957d4e2cf6f63e8
Pabitra Dhara <pabitradharardh@gmail.com>
2023-11-02 23:44:13 +0530

    add

---
+++ Practice/PabitraCode/Linkedlist/DoubleLinkedList/DoubleBasic.java
@@ -0,0 +1,50 @@
+package DoubleLinkedList;
+
+public  class DoubleBasic {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public static void displayPrev(Node tail){
+        Node temp=tail;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.prev;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+        Node f=new Node(6);
+        a.prev=null;
+        a.next=b;
+        b.prev=a;
+        b.next=c;
+        c.prev=b;
+        c.next=d;
+        d.prev=c;
+        d.next=e;
+        e.prev=d;
+        e.next=f;
+        f.prev=e;
+        f.next=null;
+        display(a);
+        displayPrev(f);
+    }
+}
