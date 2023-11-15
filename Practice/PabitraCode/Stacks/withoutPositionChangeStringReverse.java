84613ca414898601adcb077f1bf539c6a3b0e66e
Pabitra Dhara <pabitradharardh@gmail.com>
2023-11-15 23:05:51 +0530

    add

---
+++ Practice/PabitraCode/Stacks/withoutPositionChangeStringReverse.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+///a(b)c
+//output- cba
+public class withoutPositionChangeStringReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
+}
