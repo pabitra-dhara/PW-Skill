ae85e27c076ad3d6ae8c35f61b060943553eeb1a
Pabitra Dhara <pabitradharardh@gmail.com>
2023-11-26 23:55:39 +0530

    add

---
+++ Practice/PabitraCode/CollectionsFramework/interfaceImpl.java
@@ -0,0 +1,13 @@
+public class interfaceImpl {
+    public interface car {
+        void print(); 
+    }
+    class thar implements car{
+        public void print(){
+            System.out.println("Hello Pabitra Dhara");
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
