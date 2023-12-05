fe09c0b0c5c6673334f475ca00152ffbe56b952f
Pabitra Dhara <pabitradharardh@gmail.com>
2023-12-05 17:21:24 +0530

    add

--- Practice/PabitraCode/CollectionsFramework/queueinterface.java
+++ Practice/PabitraCode/CollectionsFramework/queueinterface.java
@@ -1,12 +1,31 @@
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class queueinterface {
+    public static void priorityQueueExam(){
+        //min priority queue
+        //PriorityQueue<Integer> qe=new PriorityQueue<>();
+        //max priority queue
+        PriorityQueue<Integer> qe=new PriorityQueue<>(Comparator.reverseOrder());
+        qe.add(1);
+        qe.add(5);
+        qe.add(2);
+        qe.add(4);
+        qe.add(3);
+        System.out.println("PriorityQueue:");
+        System.out.println(qe);
+        System.out.println(qe.poll());
+        System.out.println(qe);
+        System.out.println(qe.peek());
+    }
     //java linkedlist queue
     public static void QueueExem(){
         LinkedList<Integer> qu=new LinkedList<>();
         qu.offer(1);
         qu.offer(2);
         qu.offer(3);
+        System.out.println("LinkedList Queue:");
         System.out.println(qu);
         System.out.println(qu.peek());
         System.out.println(qu.poll());
@@ -18,5 +37,6 @@ public class queueinterface {
     }
     public static void main(String[] args) {
         QueueExem();
+        priorityQueueExam();
     }
 }
