563e33e10fd05be8cfc81ae2c5f66f13ab0b4292
Pabitra Dhara <pabitradharardh@gmail.com>
2023-12-06 23:20:39 +0530

    add

---
+++ Practice/PabitraCode/CollectionsFramework/stackInterface.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class stackInterface {
+    public static void stackinter(){
+        Stack<Integer> st=new Stack<>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(4);
+    }
+    public static void main(String[] args) {
+        
+
+    }
+}
