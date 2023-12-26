d1598e6273579191748889a9fb6912d928d0b083
Pabitra Dhara <pabitradharardh@gmail.com>
2023-12-26 20:49:32 +0530

    add

--- Practice/PabitraCode/sort/countSort.java
+++ Practice/PabitraCode/sort/countSort.java
@@ -24,6 +24,33 @@ public class countSort {
             }
         }
     }
+    static void countSortMain(int arr[]){
+        int max=maxfind(arr);
+        int n=arr.length;
+        int output[]=new int[n];
+        //prefix sum find use to this arr
+        int count[]=new int[max+1];
+        //1 st creat a frequency arraya value
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //2nd find prefix sum 
+        for(int i=1;i<count.length;i++){
+            count[i]=count[i]+count[i-1];
+        }
+        
+        //find the index of each element in the original array out it in output arraya
+        for(int i=n-1;i>=0;i--){
+            int idx=count[arr[i]]-1;
+            output[idx]=arr[i];
+            count[arr[i]]--;
+        }
+
+        //copy in original arr
+        for(int i=0;i<arr.length;i++){
+            arr[i]=output[i];
+        }
+    }
     static void display(int arr[]){
         for(int val:arr){
             System.out.print(val+" ");
@@ -40,7 +67,9 @@ public class countSort {
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
-        basicCountSort(arr);
+        //basicCountSort(arr);
+        //display(arr);
+        countSortMain(arr);
         display(arr);
     }
 }
