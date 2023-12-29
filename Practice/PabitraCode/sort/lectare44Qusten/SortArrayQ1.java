4bd1f9ca39c90f8b3ed2cda416f5db09b987fda6
Pabitra Dhara <pabitradharardh@gmail.com>
2023-12-29 20:25:40 +0530

    solving

---
+++ Practice/PabitraCode/sort/lectare44Qusten/SortArrayQ1.java
@@ -0,0 +1,40 @@
+package lectare44Qusten;
+
+import java.util.Scanner;
+
+public class SortArrayQ1 {
+    static void sortArr(int arr[],int size){
+        int x=-1,y=-1;
+        for(int i=1;i<size;i++){
+            if(arr[i-1]>arr[i]){
+                if(x==-1){
+                  x=i-1; 
+                  y=i; 
+                }else{
+                    y=i;
+                }
+            }
+        }
+        int temp=arr[x];
+        arr[x]=arr[y];
+        arr[y]=temp;
+    }
+    public static void display(int arr[]){
+        for(int var:arr){
+            System.out.print(var+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Array size:");
+        int size=sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        sortArr(arr, size);
+        display(arr);
+    }
+}
