fe5672dcb85a2b889d99c33f079e6cebe86d3ec4
Pabitra Dhara <pabitradharardh@gmail.com>
2024-01-02 21:19:49 +0530

    add

---
+++ Practice/PabitraCode/Search/BinarySearch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    static int Searching(int arr[],int k){
+        int start=0;
+        int end=arr.length;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==k){
+                return mid;
+            }else if(k<arr[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Array Size:");
+        int size=sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter Searching Element:");
+        int k=sc.nextInt();
+        int n=Searching(arr, k);
+        if(n<0){
+            System.out.println("Element is not found!");
+        }else{
+            System.out.println("Element found is index no:"+n);
+        }
+    }
+}
---
+++ Practice/PabitraCode/Search/BinarySearchUsingRecursion.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BinarySearchUsingRecursion {
+    public static boolean recbinearySearch(int arr[],int start,int end,int target){
+        if(start>end){
+            return false;
+        }
+        int mid=(start+end)/2;
+        if(target==arr[mid]) return true;
+        else if(target>arr[mid]) return recbinearySearch(arr, start+1, end, target);
+        else return recbinearySearch(arr, start, end-1, target);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Array Size:");
+        int size=sc.nextInt();
+        System.out.println("Enter Array Element:");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter Searching Element:");
+        int k=sc.nextInt();
+        System.out.println(recbinearySearch(arr, 0, size, k));
+    }
+}
