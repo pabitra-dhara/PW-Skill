499c2653d6b96c93df4cc1c88c11548fb9c85dd5
Pabitra Dhara <pabitradharardh@gmail.com>
2024-01-04 19:11:58 +0530

    add

---
+++ Practice/PabitraCode/Search/findSquareRoot.java
@@ -0,0 +1,24 @@
+public class findSquareRoot {
+    static int sqar(int x){
+        int st=0;
+        int end=x;
+        int ans=-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            int val=mid*mid;
+            if(mid==x){
+                return mid;
+            }else if(val<x){
+                ans=mid;
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int x=24;
+        System.out.println(sqar(x));
+    }
+}
