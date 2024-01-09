55dec465f6617bd58188371d0c8bb402b1cd5866
Pabitra Dhara <pabitradharardh@gmail.com>
2024-01-09 21:18:57 +0530

    add

--- Practice/PabitraCode/Search/findtargetElementInRotateArr.java
+++ Practice/PabitraCode/Search/findtargetElementInRotateArr.java
@@ -1,20 +1,30 @@
 public class findtargetElementInRotateArr {
-    static int findTaget(int a[],int k){
-        int n=a.length;
+    static int findTarget(int a[],int k){
         int st=0;
         int end=a.length-1;
-        int ans=-1;
         while(st<=end){
             int mid=st+(end-st)/2;
-            if(a[mid]<a[end]){
-                if(k>a[mid])
+            if(k==a[mid]){
+               return mid; 
+            }else if(a[mid]<a[end]){//mid to end arraya is sorted
+                if(k>a[mid] && k<=a[end]){
+                    st=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }else{//st to mid array is sorted
+                if(k>=a[st] && k<a[mid]){
+                    end=mid-1;
+                }else{
+                    st=mid+1;
+                }
             }
         }
-        return ans;
+        return -1;
     }
     public static void main(String[] args) {
-        int arr[]={3,4,5,1,2};
+        int arr[]={4,5,1,2,3};
         int target=3;
-        System.out.println(findTaget(arr,target));
+        System.out.println(findTarget(arr,target));
     }
 }
